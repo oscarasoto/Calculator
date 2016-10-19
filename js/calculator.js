89890e278a7d0e1a5aa9ebbe98fd672d57fefaa2
Oscar Soto <oscarasotoa@gmail.com>
2016-10-19 10:49:53 -0500

    Improve code

--- js/calculator.js
+++ js/calculator.js
@@ -1,80 +1,92 @@
 (function () {
     "use strict";
 
-    // Prints values in the Inputs
+// Check functions
+    function isARightSideDigit(objectJustClicked) {
+        return !!(document.getElementById("operator").value != "" && objectJustClicked.getAttribute("class") == "col-xs-3 digit");
+    }
+    function isALeftSideDigit(objectJustClicked) {
+        return !!(objectJustClicked.getAttribute("class") == "col-xs-3 digit" && document.getElementById("rightInput").value == "");
+
+    }
+    function isAnOperator(objectJustClicked) {
+        return !!(objectJustClicked.getAttribute("class") == "col-xs-3 operator" && document.getElementById("leftInput").value != "");
+    }
+
+// Prints values in the Inputs
     function handleDigitLeftSide(digit) {
         var display = document.getElementById("leftInput");
         display.value += digit.innerText;
-        return display.value;
     }
 
     function handleOperator(digit) {
         var display = document.getElementById("operator");
         display.value = digit.innerText;
-        return display.value;
     }
 
     function handleDigitRightSide(digit) {
         var display = document.getElementById("rightInput");
         display.value += digit.innerText;
-        return display.value;
     }
 
-    // Calculate the math for each type of operation
+// Calculate the math for each type of operation
     function calculate(left, operator, right) {
+
         var total = 0;
-        switch (operator.value){
+        switch (operator){
             case "+" :
-                total = parseFloat(left.value) + parseFloat(right.value);
+                total = parseFloat(left) + parseFloat(right);
                 break;
             case "-" :
-                total = parseFloat(left.value) - parseFloat(right.value);
+                total = parseFloat(left) - parseFloat(right);
                 break;
             case "x" :
-                total = parseFloat(left.value) * parseFloat(right.value);
+                total = parseFloat(left) * parseFloat(right);
                 break;
             case "/" :
-                total = parseFloat(left.value) / parseFloat(right.value);
+                total = parseFloat(left) / parseFloat(right);
                 break;
         }
         return total;
-
     }
 
-    // Generates an event for each button
+// Generates an event for each button
     var buttons = document.getElementsByTagName("button");
-    for (var i = 0; i < buttons.length; i++){
+    for (var i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener("click", function (event) {
             event.preventDefault();
             var objectJustClicked = this;
-            if (document.getElementById("operator").value != "" && objectJustClicked.getAttribute("class") == "col-xs-3 digit") {
-                var rightInput = handleDigitRightSide(objectJustClicked);
+
+            if (isARightSideDigit(objectJustClicked)) {
+                handleDigitRightSide(objectJustClicked);
             }
-            if (objectJustClicked.getAttribute("class") == "col-xs-3 digit" && document.getElementById("rightInput").value == "") {
-                var leftInput = handleDigitLeftSide(objectJustClicked);
+            if (isALeftSideDigit(objectJustClicked)) {
+                handleDigitLeftSide(objectJustClicked);
             }
-            if (objectJustClicked.getAttribute("class") == "col-xs-3 operator" && document.getElementById("leftInput").value != "") {
-                var operator = handleOperator(objectJustClicked);
+            if (isAnOperator(objectJustClicked)) {
+                handleOperator(objectJustClicked);
             }
         });
     }
 
-    // Sends information to perform the math and prints the result in the left side
+// Sends information to perform the math and prints the result in the left side
     var equal = document.getElementById("equal");
     equal.addEventListener("click", function (event) {
         event.preventDefault();
-
-        if (document.getElementById("leftInput").value != "" && document.getElementById("rightInput").value != "")
+        var leftInput = document.getElementById("leftInput").value;
+        var operator = document.getElementById("operator").value;
+        var rightInput = document.getElementById("rightInput").value;
+        if (document.getElementById("leftInput").value != "" && document.getElementById("rightInput").value != ""){
             document.getElementById("leftInput").value = calculate(leftInput,operator,rightInput);
             document.getElementById("operator").value = "";
             document.getElementById("rightInput").value = "";
+        }
     });
 
-    // Clears inputs
+// Clears inputs
     var clear = document.getElementById("clear");
     clear.addEventListener("click", function (event) {
          event.preventDefault();
-
          document.getElementById("leftInput").value = "";
          document.getElementById("operator").value = "";
          document.getElementById("rightInput").value = "";
